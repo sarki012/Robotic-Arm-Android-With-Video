043fb87f98d129a6e6888823b2a1088fe6dd2fc2
sarki012 <60797267+sarki012@users.noreply.github.com>
2022-11-03 13:14:08 -0500

    Working Buttons, Bluetooth, and Video Stream
    Holding the up arrow sends 'u' over Bluetooth to the Raspberry Pi, 'd' for down, 'rec' for record, etc.

--- Robotic Arm Module/java/roboticarm/Assets.java
+++ Robotic Arm Module/java/roboticarm/Assets.java
@@ -3,6 +3,7 @@ package com.esark.roboticarm;
 import com.esark.framework.Pixmap;
 
 public class Assets {
+    public static Pixmap robotPortraitBackground;
     public static Pixmap excavatorTabletLandscapeBackground;
     public static Pixmap redJoystick;
     public static Pixmap blueJoystick;
